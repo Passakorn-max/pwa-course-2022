4353a94e3c04f3d15cca29102273135a33287836
LAPTOP-I693859V\max <totemo_ahou@hotmail.com>
2022-02-25 19:54:26 +0700

    add page

--- src/Setting.js
+++ src/Setting.js
@@ -1,13 +1,13 @@
-import './css/MiniGame.css';
+import './css/Setting.css';
 
 function Setting() {
     return (
         <div>
+            <div className="Size">Size</div>
             <div className="Sound">Sound</div>
             <div className="Theme">Theme</div>
-            <div className="Size">Siza</div>
         </div>
     )
 }
 
-export default Minigame;
+export default Setting;
